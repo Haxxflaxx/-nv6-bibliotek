059579500e6a43ba4c7eed3a7ac8d7978b041d6a
Daniel Arnqvist <daniel.arnqvist@gmail.com>
2018-10-08 16:21:45 +0200

    Add lib power and test
    Missing makefile

---
+++ libpower/test.c
@@ -0,0 +1,27 @@
+#include "power.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("---Test libpower---\n");
+
+    printf("calc_power_r: ");
+    if(calc_power_r(12, 4) == 36)
+    {
+        printf("[OK]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("calc_power_i: ");
+    if(calc_power_i(12, 4) == 48)
+    {
+        printf("[OK]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+}
