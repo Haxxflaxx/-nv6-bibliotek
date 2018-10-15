429e9fc2e396c25ae1af92e5edaf855f4798fe55
Jonas Svensson <josv0198@student.umu.se>
2018-10-15 20:09:54 +0200

    Added expected results for test of libresistance

--- libresistance/test.c
+++ libresistance/test.c
@@ -14,10 +14,12 @@ int main(int argc, char**argv) {
   float floats[3];
   
   /* test array null */
+  printf("Expected Result null array: -1.000000\n");
   res = calc_resistance(3, 'P', NULL);
   printf("Result null array: %f\n", res);
   
   /* test bad data 1 */
+  printf("Expected Result bad data 1: -1.000000\n");
   floats[0] = 100.0;
   floats[1] = 200.0;
   floats[2] = 300.0;
@@ -25,6 +27,7 @@ int main(int argc, char**argv) {
   printf("Result bad data 1: %f\n", res);
 
   /* test bad data 2 */
+  printf("Expected Result bad data 2: -1.000000\n");
   floats[0] = 100.0;
   floats[1] = 0.0;
   floats[2] = 300.0;
@@ -32,6 +35,7 @@ int main(int argc, char**argv) {
   printf("Result bad data 2: %f\n", res);
 
   /* test serial */
+  printf("Expected Result serial 1: 600.000000\n");
   floats[0] = 100.0;
   floats[1] = 200.0;
   floats[2] = 300.0;
@@ -39,6 +43,7 @@ int main(int argc, char**argv) {
   printf("Result serial 1: %f\n", res);
 
   /* test parallell */
+  printf("Expected Result parallell 1: 54.545456\n");
   floats[0] = 100.0;
   floats[1] = 200.0;
   floats[2] = 300.0;
@@ -46,6 +51,7 @@ int main(int argc, char**argv) {
   printf("Result parallell 1: %f\n", res);
 
   /* test parallell zero */
+  printf("Expected Result parallell zero: 0.000000\n");
   floats[0] = 100.0;
   floats[1] = 0.0;
   floats[2] = 300.0;
